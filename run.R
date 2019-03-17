3a45ed4f6b6cd0fcb68f75312ec7435198b0e060
Superchordate <superchordate@gmail.com>
2019-03-17 14:50:50 -0500

    No scientific notation or verbose function runs

--- run.R
+++ run.R
@@ -3,11 +3,14 @@ require(bc)
 setwd( dirname( rstudioapi::getSourceEditorContext()$path ) )
 rm( list = ls() ) # clear variables to reset workspace.
 
+# no scientific notation
+options( scipen = 999 )
+
 require(magrittr)
 require(purrr)
 require(lubridate)
 
 library(dplyr)
 
-bc::runfolder( 'functions' )
+bc::runfolder( 'functions', verbose = FALSE )
 bc::runfolder( 'scripts' )
